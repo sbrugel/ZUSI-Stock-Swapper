6c9ca44474a9faf30c5eb0aae16c8cf1db92894d
Simon Brugel <sbrugel62@gmail.com>
2021-12-02 12:17:13 -0500

    feat: functioning GUI

--- ZUSI Stock Swapper/Form1.Designer.cs
+++ ZUSI Stock Swapper/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // label1
@@ -58,6 +59,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Open";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -71,7 +73,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(339, 37);
+            this.label2.Location = new System.Drawing.Point(449, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(138, 13);
             this.label2.TabIndex = 3;
@@ -80,9 +82,9 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(342, 53);
+            this.listBox1.Location = new System.Drawing.Point(208, 53);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(135, 186);
+            this.listBox1.Size = new System.Drawing.Size(580, 186);
             this.listBox1.TabIndex = 4;
             // 
             // label3
@@ -102,6 +104,7 @@
             this.button2.TabIndex = 6;
             this.button2.Text = "Open";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
@@ -137,6 +140,11 @@
             this.button3.TabIndex = 10;
             this.button3.Text = "Swap";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
@@ -174,6 +182,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ZUSI Stock Swapper/Form1.cs
+++ ZUSI Stock Swapper/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ZUSI_Stock_Swapper
 {
@@ -16,5 +12,141 @@ namespace ZUSI_Stock_Swapper
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Open a file dialog pertaining to the consist being swapped
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "TRN File|*.trn";
+            openFileDialog1.Title = "Open train";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+
+                XmlDocument doc = new XmlDocument();
+                doc.PreserveWhitespace = true;
+                doc.Load(openFileDialog1.FileName);
+                XmlNodeList elemList = doc.GetElementsByTagName("FahrzeugInfo"); //get all components of the consist
+
+                foreach (XmlNode elem in elemList)
+                {
+                    listBox1.Items.Add(elem["Datei"].Attributes["Dateiname"].Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Open a file dialog pertaining to the item to swap to
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("You haven't selected an item to swap yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            } else
+            {
+                string selected = listBox1.SelectedItem.ToString();
+                string trainType = substringFromTo(selected, indexOfNth(selected, "\\", 2)+1, indexOfNth(selected, "\\", 3)); //get the type of fragment (e.g. Electroloks)
+
+                openFileDialog1.Filter = "FZG File|*.fzg";
+                openFileDialog1.Title = "Open fragment";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.PreserveWhitespace = true;
+                    doc.Load(openFileDialog1.FileName);
+                    XmlNodeList elemList = doc.GetElementsByTagName("FahrzeugVariante"); //get all components of the consist
+
+                    foreach (XmlNode elem in elemList)
+                    {
+                        string replacement = elem["DateiAussenansicht"].Attributes["Dateiname"].Value;
+                        string replaceType = substringFromTo(replacement, indexOfNth(selected, "\\", 2) + 1, indexOfNth(replacement, "\\", 3)); //get the type of fragment (e.g. Electroloks)
+                        if (!replaceType.Equals(trainType)) //train to be replaced and replacement train are not same type
+                        {
+                            MessageBox.Show("Fragment types do not match.\nYour selected item is part of the " + trainType + " family but the replacement you selected is " +
+                                "part of the " + replaceType + " family.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        } else
+                        {
+                            textBox2.Text = openFileDialog1.FileName;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Perform the swap
+        /// </summary>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            doc.Load(textBox1.Text); //load original train file
+            XmlNodeList elemList = doc.GetElementsByTagName("FahrzeugInfo"); //get all components of the consist
+
+            foreach (XmlNode elem in elemList)
+            {
+                if (elem["Datei"].Attributes["Dateiname"].Value.Contains(listBox1.SelectedItem.ToString())) {
+                    elem["Datei"].Attributes["Dateiname"].Value.Replace(listBox1.SelectedItem.ToString(), textBox2.Text);
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Encoding = new UTF8Encoding(false); //false means do not emit the BOM - emitting it will make the .bin files completely empty
+                    using (TextWriter sw = new StreamWriter(textBox1.Text, false, new UTF8Encoding(false)))
+                    {
+                        doc.Save(sw);
+                    }
+                    MessageBox.Show("Item has been replaced");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds the nth index of a substring in a string.
+        /// </summary>
+        /// <param name="str">The string to search.</param>
+        /// <param name="value">The substring to look for.</param>
+        /// <param name="nth">What occurence of the substring to find.</param>
+        /// <returns>An integer corresponding to the position of the nth index of value in str</returns>
+        public static int indexOfNth(string str, string value, int nth = 0)
+        {
+            if (nth < 0)
+                throw new ArgumentException("Can not find a negative index of substring in string. Must start with 0");
+
+            int offset = str.IndexOf(value);
+            for (int i = 0; i < nth; i++)
+            {
+                if (offset == -1) return -1;
+                offset = str.IndexOf(value, offset + 1);
+            }
+
+            return offset;
+        }
+
+        /// <summary>
+        /// In complement with the above, finds substring between indices (if that's how the plural is spelt!)
+        /// </summary>
+        /// <param name="str">The string to search.</param>
+        /// <param name="from">The starting index of the string to search</param>
+        /// <param name="to">The ending index of the string, stop searching here</param>
+        /// <returns>The substring of str from the start to end index specified by the code</returns>
+        public string substringFromTo(string str, int from, int to)
+        {
+            try
+            {
+                if (str.Substring(from, to - from).Equals(""))
+                {
+                    return "None";
+                }
+                return str.Substring(from, to - from);
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
     }
 }
