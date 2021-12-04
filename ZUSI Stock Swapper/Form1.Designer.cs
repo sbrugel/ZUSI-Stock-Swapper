86abbc0da02cd3eb729474bdcdb2c8133c382cf4
sbrugel <sbrugel62@gmail.com>
2021-12-04 13:30:07 -0500

    feat: variation selection; German localization

--- ZUSI Stock Swapper/Form1.Designer.cs
+++ ZUSI Stock Swapper/Form1.Designer.cs
@@ -40,6 +40,9 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -53,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 8);
+            this.button1.Location = new System.Drawing.Point(208, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -63,17 +66,17 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(208, 10);
+            this.textBox1.Location = new System.Drawing.Point(289, 10);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(580, 20);
+            this.textBox1.Size = new System.Drawing.Size(715, 20);
             this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(449, 37);
+            this.label2.Location = new System.Drawing.Point(355, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(138, 13);
             this.label2.TabIndex = 3;
@@ -84,7 +87,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(208, 53);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(580, 186);
+            this.listBox1.Size = new System.Drawing.Size(428, 186);
             this.listBox1.TabIndex = 4;
             // 
             // label3
@@ -98,7 +101,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 243);
+            this.button2.Location = new System.Drawing.Point(208, 243);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
@@ -108,10 +111,10 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(208, 245);
+            this.textBox2.Location = new System.Drawing.Point(289, 245);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(580, 20);
+            this.textBox2.Size = new System.Drawing.Size(715, 20);
             this.textBox2.TabIndex = 7;
             // 
             // label4
@@ -136,7 +139,7 @@
             // 
             this.button3.Location = new System.Drawing.Point(16, 307);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(772, 23);
+            this.button3.Size = new System.Drawing.Size(988, 23);
             this.button3.TabIndex = 10;
             this.button3.Text = "Swap";
             this.button3.UseVisualStyleBackColor = true;
@@ -146,11 +149,42 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(642, 53);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(362, 186);
+            this.listBox2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(774, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Select variant";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Deutsch";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 339);
+            this.ClientSize = new System.Drawing.Size(1016, 339);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -163,7 +197,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ZUSI 3 Stock Swapper";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,6 +217,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ZUSI Stock Swapper/Form1.cs
+++ ZUSI Stock Swapper/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -9,9 +10,13 @@ namespace ZUSI_Stock_Swapper
     public partial class Form1 : Form
     {
         string tempPath = Path.GetTempPath(); //the PCs temporary file directory
+        List<string> idHaupt = new List<string>();
+        Dictionary<Control, string> deutsch = new Dictionary<Control, string>();
+        Dictionary<Control, string> english = new Dictionary<Control, string>();
         public Form1()
         {
             InitializeComponent();
+            initDictionary();
         }
 
         /// <summary>
@@ -45,7 +50,14 @@ namespace ZUSI_Stock_Swapper
         {
             if (listBox1.SelectedIndex == -1)
             {
-                MessageBox.Show("You haven't selected an item to swap yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                if (button4.Text != "English") //current language is English
+                {
+                    MessageBox.Show("You haven't selected an item to swap yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                else
+                {
+                    MessageBox.Show("Sie haben noch keinen Gegenstand zum Tauschen ausgewählt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
             } else
             {
                 string selected = listBox1.SelectedItem.ToString();
@@ -67,11 +79,22 @@ namespace ZUSI_Stock_Swapper
                         string replaceType = substringFromTo(replacement, indexOfNth(replacement, "\\", 2) + 1, indexOfNth(replacement, "\\", 3)); //get the type of fragment (e.g. Electroloks)
                         if (!replaceType.Equals(trainType)) //train to be replaced and replacement train are not same type
                         {
-                            MessageBox.Show("Fragment types do not match.\nYour selected item is part of the " + trainType + " family but the replacement you selected is " +
+                            if (button4.Text != "English") //current language is English
+                            {
+                                MessageBox.Show("Fragment types do not match.\nYour selected item is part of the " + trainType + " family but the replacement you selected is " +
                                 "part of the " + replaceType + " family.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Die Fragmenttypen stimmen nicht überein.\nIhr ausgewähltes Element ist Teil des " + trainType + " Familie, aber der von Ihnen gewählte Ersatz ist " +
+                                "teil der " + replaceType + " Familie.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
                             break;
                         } else
                         {
+                            //list all variants of the loco
+                            listBox2.Items.Add(elem.Attributes["Beschreibung"].Value + " (" + elem.Attributes["Farbgebung"].Value + ")");
+                            idHaupt.Add(elem.Attributes["IDHaupt"].Value);
                             textBox2.Text = openFileDialog1.FileName;
                         }
                     }
@@ -84,32 +107,55 @@ namespace ZUSI_Stock_Swapper
         /// </summary>
         private void button3_Click(object sender, EventArgs e)
         {
-            XmlDocument doc = new XmlDocument();
-            doc.PreserveWhitespace = true;
-            doc.Load(textBox1.Text); //load original train file
-            XmlNodeList elemList = doc.GetElementsByTagName("FahrzeugInfo"); //get all components of the consist
-
-            foreach (XmlNode elem in elemList)
+            if (listBox2.SelectedIndex != -1)
             {
-                if (elem["Datei"].Attributes["Dateiname"].Value.Contains(listBox1.SelectedItem.ToString())) {
-                    //save the original folder in Temp (in case the new .trn file doesn't work and/or user wants to revert)
-                    if (!Directory.Exists(tempPath + "ZSS"))
+                XmlDocument doc = new XmlDocument();
+                doc.PreserveWhitespace = true;
+                doc.Load(textBox1.Text); //load original train file
+                XmlNodeList elemList = doc.GetElementsByTagName("FahrzeugInfo"); //get all components of the consist
+
+                foreach (XmlNode elem in elemList)
+                {
+                    if (elem["Datei"].Attributes["Dateiname"].Value.Contains(listBox1.SelectedItem.ToString()))
                     {
-                        Directory.CreateDirectory(tempPath + "ZSS");
+                        //save the original folder in Temp (in case the new .trn file doesn't work and/or user wants to revert)
+                        if (!Directory.Exists(tempPath + "ZSS"))
+                        {
+                            Directory.CreateDirectory(tempPath + "ZSS");
+                        }
+                        using (TextWriter sw = new StreamWriter(tempPath + "ZSS\\" + substringFromTo(textBox1.Text, textBox1.Text.LastIndexOf("\\") + 1, textBox1.Text.Length), false, new UTF8Encoding(false)))
+                        {
+                            doc.Save(sw);
+                        }
+                        elem["Datei"].Attributes["Dateiname"].Value = elem["Datei"].Attributes["Dateiname"].Value.Replace(listBox1.SelectedItem.ToString(),
+                            substringFromTo(textBox2.Text, textBox2.Text.IndexOf("RollingStock"), textBox2.Text.Length));
+                        elem.Attributes["IDHaupt"].Value = idHaupt[listBox2.SelectedIndex];
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Encoding = new UTF8Encoding(false); //false means do not emit the BOM - emitting it will make the .bin files completely empty
+                        using (TextWriter sw = new StreamWriter(textBox1.Text, false, new UTF8Encoding(false)))
+                        {
+                            doc.Save(sw);
+                        }
+                        if (button4.Text != "English") //current language is English
+                        {
+                            MessageBox.Show("Item has been replaced.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Der Gegenstand wurde ersetzt.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
                     }
-                    using (TextWriter sw = new StreamWriter(tempPath + "ZSS\\" + substringFromTo(textBox1.Text, textBox1.Text.LastIndexOf("\\") + 1, textBox1.Text.Length), false, new UTF8Encoding(false)))
-                    {
-                        doc.Save(sw);
-                    }
-                    elem["Datei"].Attributes["Dateiname"].Value = elem["Datei"].Attributes["Dateiname"].Value.Replace(listBox1.SelectedItem.ToString(), textBox2.Text);
-                    XmlWriterSettings settings = new XmlWriterSettings();
-                    settings.Encoding = new UTF8Encoding(false); //false means do not emit the BOM - emitting it will make the .bin files completely empty
-                    using (TextWriter sw = new StreamWriter(textBox1.Text, false, new UTF8Encoding(false)))
-                    {
-                        doc.Save(sw);
-                    }
-                    MessageBox.Show("Item has been replaced");
                 }
+            } else
+            {
+                if (button4.Text != "English") //current language is English
+                {
+                    MessageBox.Show("You haven't selected the variant of the item to swap yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                } else
+                {
+                    MessageBox.Show("Sie haben noch nicht die Variante des zu tauschenden Artikels ausgewählt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                
             }
         }
 
@@ -158,5 +204,50 @@ namespace ZUSI_Stock_Swapper
             }
         }
 
+        private void initDictionary()
+        {
+            deutsch.Add(button1, "Offen");
+            deutsch.Add(button2, "Offen");
+            deutsch.Add(button3, "Tauschen Sie");
+            deutsch.Add(button4, "English");
+
+            english.Add(button1, "Open");
+            english.Add(button2, "Open");
+            english.Add(button3, "Swap");
+            english.Add(button4, "Deutsch");
+
+            deutsch.Add(label1, "Zu ändernden Zug auswählen");
+            deutsch.Add(label2, "Zu ändernde Komponente auswählen");
+            deutsch.Add(label3, "Tausch mit");
+            deutsch.Add(label4, "Der zu tauschende Gegenstand muss vom gleichen Typ sein wie der Gegenstand, mit dem getauscht wird");
+            deutsch.Add(label5, "Sie können zum Beispiel eine Diesellokomotive nicht mit einer Elektrolokomotive austauschen.");
+            deutsch.Add(label6, "Variante auswählen");
+
+            english.Add(label1, "Select train to modify:");
+            english.Add(label2, "Select component to modify");
+            english.Add(label3, "Swap with:");
+            english.Add(label4, "The item you will swap to must be the same type as the item being swapped.");
+            english.Add(label5, "i.e. You cannot swap a diesel locomotive with an electric locomotive.");
+            english.Add(label6, "Select variant");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text.Equals("Deutsch"))
+            {
+                button4.Text = "English";
+                foreach (KeyValuePair<Control, string> kvp in deutsch)
+                {
+                    kvp.Key.Text = kvp.Value;
+                }
+            } else
+            {
+                button4.Text = "Deutsch";
+                foreach (KeyValuePair<Control, string> kvp in english)
+                {
+                    kvp.Key.Text = kvp.Value;
+                }
+            }
+        }
     }
 }
